732535699b2fc9f6c9f672e6ad19e20191c9792d
nastiavynohradova <anastasivynohradova@gmail.com>
2024-01-02 12:30:14 +0100

    Updates

---
+++ precache-manifest.6c6f9c9ee4c93137af0efb65575cc808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163d84d907cc1afa22b12872969d72ef",
+    "url": "/gv-targeterfassung-test/index.html"
+  },
+  {
+    "revision": "ec1ef05b935df5034420",
+    "url": "/gv-targeterfassung-test/static/css/main.21d7bd9c.chunk.css"
+  },
+  {
+    "revision": "778e0fbcd45d469325b2",
+    "url": "/gv-targeterfassung-test/static/js/2.eedc13a2.chunk.js"
+  },
+  {
+    "revision": "85c33a3242cdab197985f6af46cf90f6",
+    "url": "/gv-targeterfassung-test/static/js/2.eedc13a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1ef05b935df5034420",
+    "url": "/gv-targeterfassung-test/static/js/main.f6c56263.chunk.js"
+  },
+  {
+    "revision": "3b8475ddee44082c9fe4",
+    "url": "/gv-targeterfassung-test/static/js/runtime-main.8ea3e880.js"
+  }
+]);
