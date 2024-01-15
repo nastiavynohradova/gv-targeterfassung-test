fce62324a239bebb1970125c300297eb89c26a88
nastiavynohradova <anastasivynohradova@gmail.com>
2024-01-15 14:59:44 +0100

    Updates

--- precache-manifest.97836717a7d69c31245cc021f55046d5.js
+++ precache-manifest.97836717a7d69c31245cc021f55046d5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0674f0c0a6bd8b7fba034b82d3069529",
+    "revision": "80ec5c28a32d98b96ddb84f4a280afd1",
     "url": "/gv-targeterfassung-test/index.html"
   },
   {
-    "revision": "70e3181c8b344dc3f007",
+    "revision": "791dedc479f8b9246868",
     "url": "/gv-targeterfassung-test/static/css/main.21d7bd9c.chunk.css"
   },
   {
-    "revision": "38c46fa682758aaad735",
-    "url": "/gv-targeterfassung-test/static/js/2.80b6a974.chunk.js"
+    "revision": "5459fd24c08c4be1800a",
+    "url": "/gv-targeterfassung-test/static/js/2.f3c53d62.chunk.js"
   },
   {
     "revision": "ca43cbaa8b0fadb49fafd64c87198872",
-    "url": "/gv-targeterfassung-test/static/js/2.80b6a974.chunk.js.LICENSE.txt"
+    "url": "/gv-targeterfassung-test/static/js/2.f3c53d62.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "70e3181c8b344dc3f007",
-    "url": "/gv-targeterfassung-test/static/js/main.159ea6eb.chunk.js"
+    "revision": "791dedc479f8b9246868",
+    "url": "/gv-targeterfassung-test/static/js/main.fe2ca0a4.chunk.js"
   },
   {
     "revision": "3b8475ddee44082c9fe4",
